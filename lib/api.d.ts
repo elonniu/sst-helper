0862a18ff265b907b6817249c4cce29765d9ecc1
Elon Niu <elonniu@amazon.com>
2023-06-04 15:31:04 +0800

    improved response methods

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -1,8 +1,11 @@
-export declare function response(json: Object): {
+export declare function param(value: string | undefined | number, key: string, defaultValue?: string | number | null): string | number;
+export declare function jsonResponse(json: object): {
+    isBase64Encoded: boolean;
     statusCode: number;
     headers: {
-        "Content-Type": string;
+        'Content-Type': string;
+        'Access-Control-Allow-Origin': string;
     };
     body: string;
 };
-export declare function param(value: string | undefined | number, key: string, defaultValue?: string | number | null): string | number;
+export declare function htmlResponse(html: string): object;
--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,3 @@
-export function response(json: Object) {
-
-    return {
-        statusCode: 200,
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(json),
-    };
-
-}
 export function param(value: string | undefined | number, key: string, defaultValue: string | number | null = null) {
 
     if (value) {
@@ -18,4 +9,30 @@ export function param(value: string | undefined | number, key: string, defaultVa
     }
 
     throw new Error(`${key} can not be empty`)
+}
+
+export function jsonResponse(json: object) {
+    return {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(json)
+    };
+}
+
+export function htmlResponse(html: string): object {
+
+    return {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: html
+    };
+
 }
