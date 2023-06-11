1305c8ea73989ef623b9fabf845beecb1fe2a22e
Elon Niu <elonniu@amazon.com>
2023-06-11 12:53:10 +0800

    added time methods

--- lib/time.d.ts
+++ lib/time.d.ts
@@ -1,2 +1,5 @@
 export declare function ms(): number;
 export declare function s(): number;
+export declare function m(): number;
+export declare function h(): number;
+export declare function d(): number;
