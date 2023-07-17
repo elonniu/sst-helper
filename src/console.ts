540f65c23bf06e788fd6aebf0ecaa65c753f9666
elonniu <elonniu@amazon.com>
2023-07-17 18:02:21 +0800

    improved url len

--- src/console.ts
+++ src/console.ts
@@ -2,11 +2,11 @@ import {Api, ApiGatewayV1Api, Stack} from "sst/constructs";
 import {fixedPath} from "./url.js";
 
 export function stackUrl(stackId: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/cloudformation/home?region=${region}#/stacks/stackinfo?filteringStatus=active&filteringText=&viewNested=true&stackId=${stackId}`;
+    return `https://${region}.console.${awsDomain(region)}/cloudformation/home?#/stacks/stackinfo?stackId=${stackId}`;
 }
 
 export function cloudFrontUrl(distributionId: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/cloudfront/v3/home?region=${region}#/distributions/${distributionId}`;
+    return `https://${region}.console.${awsDomain(region)}/cloudfront/v3/home?#/distributions/${distributionId}`;
 }
 
 export function apiUrl(api: Api | ApiGatewayV1Api, stack: Stack) {
@@ -15,36 +15,36 @@ export function apiUrl(api: Api | ApiGatewayV1Api, stack: Stack) {
     // @ts-ignore
     if (api?.restApiId) {
         // @ts-ignore
-        return `https://${region}.console.${awsDomain(region)}/apigateway/home?region=${region}#/apis/${api.restApiId}/resources/`;
+        return `https://${region}.console.${awsDomain(region)}/apigateway/home?#/apis/${api.restApiId}/resources/`;
     }
 
     // @ts-ignore
-    return `https://${region}.console.${awsDomain(region)}/apigateway/main/api-detail?api=${api?.httpApiId}&region=${region}`;
+    return `https://${region}.console.${awsDomain(region)}/apigateway/main/api-detail?api=${api?.httpApiId}`;
 }
 
 export function userPoolUrl(userPoolId: string, region: string) {
 
     if (region.startsWith('cn-')) {
-        return `https://${region}.console.${awsDomain(region)}/cognito/v2/idp/user-pools/${userPoolId}/users?region=${region}`;
+        return `https://${region}.console.${awsDomain(region)}/cognito/v2/idp/user-pools/${userPoolId}/users`;
     }
 
-    return `https://${region}.console.${awsDomain(region)}/cognito/v2/idp/user-pools/${userPoolId}/users?region=${region}`;
+    return `https://${region}.console.${awsDomain(region)}/cognito/v2/idp/user-pools/${userPoolId}/users`;
 }
 
 export function identityPoolUrl(cognitoIdentityPoolId: string, region: string) {
     if (region.startsWith('cn-')) {
-        return `https://${region}.console.${region}/cognito/v2/identity/identity-pools/${cognitoIdentityPoolId}/user-statistics?region=${region}`;
+        return `https://${region}.console.${region}/cognito/v2/identity/identity-pools/${cognitoIdentityPoolId}/user-statistics`;
     }
 
-    return `https://${region}.console.${region}/cognito/v2/identity/identity-pools/${cognitoIdentityPoolId}/user-statistics?region=${region}`;
+    return `https://${region}.console.${region}/cognito/v2/identity/identity-pools/${cognitoIdentityPoolId}/user-statistics`;
 }
 
 export function s3Url(bucketName: string, region: string) {
     if (region.startsWith('cn-')) {
-        return `https://console.${awsDomain(region)}/s3/buckets/${bucketName}?region=${region}&tab=objects`;
+        return `https://console.${awsDomain(region)}/s3/buckets/${bucketName}`;
     }
 
-    return `https://s3.console.${awsDomain(region)}/s3/buckets/${bucketName}?region=${region}&tab=objects`;
+    return `https://s3.console.${awsDomain(region)}/s3/buckets/${bucketName}`;
 }
 
 export function bucketUrl(bucketName: string, region: string) {
@@ -52,47 +52,47 @@ export function bucketUrl(bucketName: string, region: string) {
 }
 
 export function ddbUrl(tableName: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/dynamodbv2/home#table?initialTagKey=&name=${tableName}`;
+    return `https://${region}.console.${awsDomain(region)}/dynamodbv2/home#table?name=${tableName}`;
 }
 
 export function sfUrl(stateMachineArn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/states/home?region=${region}#/statemachines/view/${stateMachineArn}`;
+    return `https://${region}.console.${awsDomain(region)}/states/home?#/statemachines/view/${stateMachineArn}`;
 }
 
 export function executionUrl(executionArn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/states/home?region=${region}#/v2/executions/details/${executionArn}`;
+    return `https://${region}.console.${awsDomain(region)}/states/home?#/v2/executions/details/${executionArn}`;
 }
 
 export function ec2InstanceUrl(arn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/ec2/home?region=${region}#InstanceDetails:instanceId=${arn}`;
+    return `https://${region}.console.${awsDomain(region)}/ec2/home?#InstanceDetails:instanceId=${arn}`;
 }
 
 export function fargateTaskUrl(arn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/ecs/v2/clusters/${arn.split('/')[1]}/tasks/${arn.split('/')[2]}/configuration?region=${region}&selectedContainer=TaskContainer`;
+    return `https://${region}.console.${awsDomain(region)}/ecs/v2/clusters/${arn.split('/')[1]}/tasks/${arn.split('/')[2]}`;
 }
 
 export function batchJobUrl(arn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/batch/home?region=${region}#jobs/fargate/detail/${arn}`;
+    return `https://${region}.console.${awsDomain(region)}/batch/home?#jobs/fargate/detail/${arn}`;
 }
 
 export function ddbExploreUrl(tableName: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/dynamodbv2/home?region=${region}#item-explorer?initialTagKey=&table=${tableName}`;
+    return `https://${region}.console.${awsDomain(region)}/dynamodbv2/home?#item-explorer?table=${tableName}`;
 }
 
 export function sqsUrl(queueName: string, region: string, account: string) {
-    return `https://${region}.console.${awsDomain(region)}/sqs/v2/home?region=${region}#/queues/https%3A%2F%2Fsqs.${region}.amazonaws.com%2F${account}%2F${queueName}`;
+    return `https://${region}.console.${awsDomain(region)}/sqs/v2/home?#/queues/https%3A%2F%2Fsqs.${region}.amazonaws.com%2F${account}%2F${queueName}`;
 }
 
 export function busUrl(eventBusName: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/events/home?region=${region}#/eventbus/${eventBusName}`;
+    return `https://${region}.console.${awsDomain(region)}/events/home?#/eventbus/${eventBusName}`;
 }
 
 export function topicUrl(topicArn: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/sns/v3/home?region=${region}#/topic/${topicArn}`;
+    return `https://${region}.console.${awsDomain(region)}/sns/v3/home?#/topic/${topicArn}`;
 }
 
 export function kdsUrl(streamName: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/kinesis/home?region=${region}#/streams/details/${streamName}/monitoring`;
+    return `https://${region}.console.${awsDomain(region)}/kinesis/home?#/streams/details/${streamName}/monitoring`;
 }
 
 export function distributionUrl(id: string, region: string) {
@@ -104,15 +104,15 @@ export function route53Url(hostedZoneId: string, region: string) {
 }
 
 export function lambdaUrl(functionName: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/lambda/home?region=${region}#/functions/${functionName}`;
+    return `https://${region}.console.${awsDomain(region)}/lambda/home?#/functions/${functionName}`;
 }
 
 export function lambdaLogUrl(context: any, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/cloudwatch/home?region=${region}#logsV2:log-groups/log-group/${fixedPath(context.logGroupName)}/log-events/${fixedPath(context.logStreamName)}`;
+    return `https://${region}.console.${awsDomain(region)}/cloudwatch/home?#logsV2:log-groups/log-group/${fixedPath(context.logGroupName)}/log-events/${fixedPath(context.logStreamName)}`;
 }
 
 export function apiLogUrl(apiId: string, region: string) {
-    return `https://${region}.console.${awsDomain(region)}/apigateway/main/api-detail?api=${apiId}&region=${region}`;
+    return `https://${region}.console.${awsDomain(region)}/apigateway/main/api-detail?api=${apiId}`;
 }
 
 export function awsDomain(region: string) {
