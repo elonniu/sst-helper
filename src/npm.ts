a5f5e00dbb93bfa30fbba696e26a42c771e5f130
elonniu <elonniu@amazon.com>
2023-07-19 04:14:35 +0800

    fixed npm

--- src/npm.ts
+++ src/npm.ts
@@ -1,18 +1,18 @@
 import {execSync} from "child_process";
 import {promises as fs} from "fs";
 
-export function getRoot() {
-    return execSync('npm root -g').toString().trim() + '/ibench';
+export function getRoot(packageName: string) {
+    return execSync('npm root -g').toString().trim() + '/' + packageName;
 }
 
-export async function getPackageProperty(key: string, defaultVal: any = undefined) {
-    const packages = getRoot() + '/package.json';
+export async function getPackageProperty(packageName: string, key: string, defaultVal: any = undefined) {
+    const packages = getRoot(packageName) + '/package.json';
     const packageData = await fs.readFile(packages, 'utf-8');
     const json = JSON.parse(packageData);
 
     return json[key] ? json[key] : defaultVal;
 }
 
-export async function currentVersion() {
-    return getPackageProperty('version');
+export async function currentVersion(packageName: string) {
+    return getPackageProperty(packageName, 'version');
 }
